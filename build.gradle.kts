2243829654ed653e7d11c359a1a0f85ece974697
HaeunLee <haeun930515@gmail.com>
2022-09-22 16:52:32 +0900

    버전 upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@
 plugins {
     id("com.android.application") version "7.2.1" apply false
     id("com.android.library") version "7.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
     id("com.google.dagger.hilt.android") version Versions.hilt apply false
     id("com.google.gms.google-services") version "4.3.10" apply false
     id("org.jetbrains.kotlin.jvm") version "1.7.0" apply false
+    id("com.android.dynamic-feature") version "7.2.1" apply false
 }
 
 allprojects {
